bfa0fda7b82152a28797f3a499720ef61be429a2
aarnav25bce10153-ops <aarnav.25bce10153@vitbhopal.ac.in>
2025-11-23 10:50:31 +0530

    Add files via upload

---
+++ #digit count.py
@@ -0,0 +1,25 @@
+
+        
+    
+
+
+
+
+
+
+
+#14/11/25
+n=int(input("Enter a number n"))
+a=int(input("Enter the index "))
+s=""
+for i in range (1,n+1):
+   if len(s)<n+1:
+     s=s+str(i)
+   
+    
+              
+print(s[:n]) 
+print(s[a])
+
+
+
